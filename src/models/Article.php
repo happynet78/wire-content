4702f309e69ba994714e3cdaf2eb36fb67c3e3a0
happynet78 <happynet1004@gmail.com>
2024-12-28 21:09:32 +0900

    commit

--- src/models/Article.php
+++ src/models/Article.php
@@ -2,13 +2,17 @@
 
 namespace WireContent\models;
 
+use App\Models\User;
+use Awcodes\Curator\Models\Media;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use WireComments\Traits\Commentable;
 
 class Article extends Model
 {
     use HasFactory;
+    use Commentable;
 
     protected $fillable = [
         'title',
@@ -17,8 +21,13 @@ class Article extends Model
         'media_id'
     ];
 
+    public function imgage(): BelongsTo
+    {
+        return $this->belongsTo(Media::class, 'media_id');
+    }
+
     public function user(): BelongsTo
     {
-        return $this->belongsTo(config('auth.providers.users.model'));
+        return $this->belongsTo(User::class);
     }
 }
